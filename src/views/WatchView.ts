79ef521128550316da87a3e89016ebaac6eb9cc0
Laurent Binder <laurentbinder@gmail.com>
2024-04-19 09:29:40 +0200

    toggle format WIP

--- src/views/WatchView.ts
+++ src/views/WatchView.ts
@@ -1,8 +1,23 @@
+import { WatchModel } from '../models/WatchModel';
+
 export class WatchView {
+  model: WatchModel;
+  constructor(model: WatchModel) {
+    this.model = model;
+  }
+
   displayTime(time: Date): void {
     const hours: string = time.getHours().toString().padStart(2, '0');
     const minutes: string = time.getMinutes().toString().padStart(2, '0');
     const seconds: string = time.getSeconds().toString().padStart(2, '0');
+    // const formatOptions: Intl.DateTimeFormatOptions = {
+    //   hour12: this.model.getTimeFormat().includes('A'),
+    // };
+    // const formattedTime: string = time.toLocaleTimeString(
+    //   undefined,
+    //   formatOptions
+    // );
+    // console.log('🚀 ~ WatchView ~ displayTime ~ formattedTime:', formattedTime);
 
     const hoursAndMinutesDisplay: HTMLElement | null =
       document.getElementById('hours-and-minutes');
@@ -16,4 +31,8 @@ export class WatchView {
       throw new Error('Elements for displaying time not found.');
     }
   }
+
+  // displayLightStatus(lightOn: boolean): void {
+  //   console.log(lightOn ? 'Light is on' : 'Light is off');
+  // }
 }
